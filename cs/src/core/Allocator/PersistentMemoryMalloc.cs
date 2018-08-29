be41ed8b3aba451bafdea3a6143e19e911c064a6
Badrish Chandramouli <badrishc@microsoft.com>
2018-08-29 16:34:20 -0700

    Removing CALLOC #define

--- cs/src/core/Allocator/PersistentMemoryMalloc.cs
+++ cs/src/core/Allocator/PersistentMemoryMalloc.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-#define CALLOC
 using System;
 using System.Runtime.CompilerServices;
 using System.Threading;
