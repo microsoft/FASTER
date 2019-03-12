e2614e24a32f07af8abb3d563ba1b1ef76c0d95b
Badrish Chandramouli <badrishc@microsoft.com>
2019-03-12 10:21:25 -0700

    Updated ClassCache sample.

--- cs/src/core/Index/FASTER/LogAccessor.cs
+++ cs/src/core/Index/FASTER/LogAccessor.cs
@@ -46,19 +46,17 @@ namespace FASTER.core
             allocator.ShiftBeginAddress(untilAddress);
         }
 
+
         /// <summary>
         /// Shift log head address to prune memory foorprint of hybrid log
         /// </summary>
         /// <param name="newHeadAddress">Address to shift head until</param>
         /// <param name="wait">Wait to ensure shift is registered (may involve page flushing)</param>
+        /// <returns>When wait is false, this tells whether the shift to newHeadAddress was successfully registered with FASTER</returns>
         public bool ShiftHeadAddress(long newHeadAddress, bool wait)
         {
             // First shift read-only
-            allocator.ShiftReadOnlyAddress(newHeadAddress);
-
-            // Wait for flush to complete
-            while (wait && allocator.FlushedUntilAddress < newHeadAddress)
-                fht.Refresh();
+            ShiftReadOnlyAddress(newHeadAddress, wait);
 
             // Then shift head address
             var updatedHeadAddress = allocator.ShiftHeadAddress(newHeadAddress);
@@ -70,9 +68,14 @@ namespace FASTER.core
         /// Shift log read-only address
         /// </summary>
         /// <param name="newReadOnlyAddress">Address to shift read-only until</param>
-        public bool ShiftReadOnlyAddress(long newReadOnlyAddress)
+        /// <param name="wait">Wait to ensure shift is complete (may involve page flushing)</param>
+        public void ShiftReadOnlyAddress(long newReadOnlyAddress, bool wait)
         {
-            return allocator.ShiftReadOnlyAddress(newReadOnlyAddress);
+            allocator.ShiftReadOnlyAddress(newReadOnlyAddress);
+
+            // Wait for flush to complete
+            while (wait && allocator.FlushedUntilAddress < newReadOnlyAddress)
+                fht.Refresh();
         }
 
         /// <summary>
@@ -93,24 +96,17 @@ namespace FASTER.core
         /// <param name="wait">Synchronous wait for operation to complete</param>
         public void Flush(bool wait)
         {
-            var newReadOnlyAddress = allocator.GetTailAddress();
-
-            // First shift read-only
-            allocator.ShiftReadOnlyAddress(newReadOnlyAddress);
-
-            // Wait for flush to complete
-            while (wait && allocator.FlushedUntilAddress < newReadOnlyAddress)
-                fht.Refresh();
+            ShiftReadOnlyAddress(allocator.GetTailAddress(), wait);
         }
 
         /// <summary>
         /// Flush log and evict all records from memory
         /// </summary>
         /// <param name="wait">Synchronous wait for operation to complete</param>
-        public void FlushAndEvict(bool wait)
+        /// <returns>When wait is false, this tells whether the full eviction was successfully registered with FASTER</returns>
+        public bool FlushAndEvict(bool wait)
         {
-            var newHeadAddress = allocator.GetTailAddress();
-            ShiftHeadAddress(newHeadAddress, wait);
+            return ShiftHeadAddress(allocator.GetTailAddress(), wait);
         }
 
         /// <summary>
