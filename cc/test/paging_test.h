2a7f7add1cf086b884c5458adc9cd7c8c794fc4a
Badrish Chandramouli <badrishc@microsoft.com>
2019-03-13 18:33:45 -0700

    test

--- cc/test/paging_test.h
+++ cc/test/paging_test.h
@@ -702,7 +702,7 @@ TEST(CLASS, Rmw) {
 
   Guid session_id = store.StartSession();
 
-  constexpr size_t kNumRecords = 250000;
+  constexpr size_t kNumRecords = 25000;
 
   // Initial RMW.
   static std::atomic<uint64_t> records_touched{ 0 };
