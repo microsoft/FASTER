55d68d10dbaa978ee472192153ba9be64e2c220c
Badrish Chandramouli <badrishc@microsoft.com>
2019-07-23 17:54:05 -0700

    Updated proposed API for CheckpointManager

--- cs/src/core/Index/FASTER/CheckpointManager.cs
+++ cs/src/core/Index/FASTER/CheckpointManager.cs
@@ -12,34 +12,69 @@ using System.Threading;
 
 namespace FASTER.core
 {
-    public interface ICheckpointDeviceManager
+    /// <summary>
+    /// Interface for users to control creation and retrieval of checkpoint-related data
+    /// </summary>
+    public interface ICheckpointManager
     {
         /// <summary>
-        /// Index-related devices
+        /// Device to store index checkpoint (including overflow buckets)
         /// </summary>
         /// <param name="token"></param>
         /// <returns></returns>
         IDevice GetIndexDevice(Guid token);
-        IDevice GetOverflowBucketDevice(Guid token);
-        IDevice GetIndexCheckpointInfoDevice(Guid token);
-        IDevice GetIndexCheckpointCompletedDevice(Guid token);
 
         /// <summary>
-        /// Log-related devices
+        /// Device to store snapshot of log (required only for snapshot checkpoints)
         /// </summary>
         /// <param name="token"></param>
         /// <returns></returns>
         IDevice GetSnapshotLogDevice(Guid token);
-        IDevice GetSnapshotObjectLogDevice(Guid token);
-        IDevice GetLogCheckpointInfoDevice(Guid token);
-        IDevice GetLogCheckpointCompletedDevice(Guid token);
 
         /// <summary>
-        /// Recovery helpers
+        /// Device to store snapshot of object log (required only for snapshot checkpoints)
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        IDevice GetSnapshotObjectLogDevice(Guid token);
+
+
+        /// <summary>
+        /// Commit index checkpoint (synchronous)
+        /// </summary>
+        /// <param name="indexToken"></param>
+        /// <param name="commitInfo"></param>
+        /// <returns></returns>
+        void CommitIndexCheckpoint(Guid indexToken, byte[] commitInfo);
+
+        /// <summary>
+        /// Commit log checkpoint (synchronous)
+        /// </summary>
+        /// <param name="logToken"></param>
+        /// <param name="commitInfo"></param>
+        /// <returns></returns>
+        void CommitLogCheckpoint(Guid logToken, byte[] commitInfo);
+
+        /// <summary>
+        /// Retrieve commit info for previous index checkpoint
+        /// </summary>
+        /// <param name="indexToken"></param>
+        /// <returns>Commit info, if valid checkpoint found, and null otherwise</returns>
+        byte[] GetIndexCommitInfo(Guid indexToken);
+
+        /// <summary>
+        /// Retrieve commit info for previous log checkpoint
+        /// </summary>
+        /// <param name="logToken"></param>
+        /// <returns>Commit info, if valid checkpoint found, and null otherwise</returns>
+        byte[] GetLogCommitInfo(Guid logToken);
+
+        /// <summary>
+        /// Get latest valid checkpoint
         /// </summary>
         /// <param name="indexToken"></param>
         /// <param name="logToken"></param>
-        /// <returns></returns>
-        bool GetLatestValidCheckpoint(out Guid indexToken, out Guid logToken);
+        /// <returns>true if latest valid checkpoint found, false otherwise</returns>
+        bool GetLatestCheckpoint(out Guid indexToken, out Guid logToken);
     }
 }
