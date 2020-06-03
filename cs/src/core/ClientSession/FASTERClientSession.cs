08124d428d1f1b2fa5ccde1330dc9ca163ee544d
Marius Klimantavičius <7106278+marius-klimantavicius@users.noreply.github.com>
2020-06-03 17:52:17 +0300

    Added simple test for multiple sessions

--- cs/src/core/ClientSession/FASTERClientSession.cs
+++ cs/src/core/ClientSession/FASTERClientSession.cs
@@ -16,6 +16,75 @@ namespace FASTER.core
         internal Dictionary<string, IClientSession> _activeSessions;
 
         /// <summary>
+        /// Client session type helper
+        /// </summary>
+        /// <typeparam name="Input"></typeparam>
+        /// <typeparam name="Output"></typeparam>
+        /// <typeparam name="Context"></typeparam>
+        public struct ClientSessionBuilder<Input, Output, Context>
+        {
+            private readonly FasterKV<Key, Value> _fasterKV;
+
+            internal ClientSessionBuilder(FasterKV<Key, Value> fasterKV)
+            {
+                _fasterKV = fasterKV;
+            }
+
+            /// <summary>
+            /// Start a new client session with FASTER.
+            /// </summary>
+            /// <param name="functions">Callback functions</param>
+            /// <param name="sessionId">ID/name of session (auto-generated if not provided)</param>
+            /// <param name="threadAffinitized">For advanced users. Specifies whether session holds the thread epoch across calls. Do not use with async code. Ensure thread calls session Refresh periodically to move the system epoch forward.</param>
+            /// <returns>Session instance</returns>
+            public ClientSession<Key, Value, Input, Output, Context, Functions> NewSession<Functions>(Functions functions, string sessionId = null, bool threadAffinitized = false)
+                where Functions : IFunctions<Key, Value, Input, Output, Context>
+            {
+                return _fasterKV.NewSession<Input, Output, Context, Functions>(functions, sessionId, threadAffinitized);
+            }
+
+            /// <summary>
+            /// Resume (continue) prior client session with FASTER, used during
+            /// recovery from failure.
+            /// </summary>
+            /// <param name="functions">Callback functions</param>
+            /// <param name="sessionId">ID/name of previous session to resume</param>
+            /// <param name="commitPoint">Prior commit point of durability for session</param>
+            /// <param name="threadAffinitized">For advanced users. Specifies whether session holds the thread epoch across calls. Do not use with async code. Ensure thread calls session Refresh periodically to move the system epoch forward.</param>
+            /// <returns>Session instance</returns>
+            public ClientSession<Key, Value, Input, Output, Context, Functions> ResumeSession<Functions>(Functions functions, string sessionId, out CommitPoint commitPoint, bool threadAffinitized = false)
+                where Functions : IFunctions<Key, Value, Input, Output, Context>
+            {
+                return _fasterKV.ResumeSession<Input, Output, Context, Functions>(functions, sessionId, out commitPoint, threadAffinitized);
+            }
+        }
+
+        /// <summary>
+        /// Helper method to specify Input, Output and Context for callback functions
+        /// </summary>
+        /// <typeparam name="Input"></typeparam>
+        /// <typeparam name="Output"></typeparam>
+        /// <typeparam name="Context"></typeparam>
+        /// <returns></returns>
+        public ClientSessionBuilder<Input, Output, Context> For<Input, Output, Context>()
+        {
+            return new ClientSessionBuilder<Input, Output, Context>(this);
+        }
+
+        /// <summary>
+        /// Start a new client session with FASTER.
+        /// For performance reasons this overload is not recommended if functions is value type (struct).
+        /// </summary>
+        /// <param name="functions">Callback functions</param>
+        /// <param name="sessionId">ID/name of session (auto-generated if not provided)</param>
+        /// <param name="threadAffinitized">For advanced users. Specifies whether session holds the thread epoch across calls. Do not use with async code. Ensure thread calls session Refresh periodically to move the system epoch forward.</param>
+        /// <returns>Session instance</returns>
+        public ClientSession<Key, Value, Input, Output, Context, IFunctions<Key, Value, Input, Output, Context>> NewSession<Input, Output, Context>(IFunctions<Key, Value, Input, Output, Context> functions, string sessionId = null, bool threadAffinitized = false)
+        {
+            return NewSession<Input, Output, Context, IFunctions<Key, Value, Input, Output, Context>>(functions, sessionId, threadAffinitized);
+        }
+
+        /// <summary>
         /// Start a new client session with FASTER.
         /// </summary>
         /// <param name="functions">Callback functions</param>
@@ -48,6 +117,21 @@ namespace FASTER.core
             lock (_activeSessions)
                 _activeSessions.Add(sessionId, session);
             return session;
+        }
+
+        /// <summary>
+        /// Resume (continue) prior client session with FASTER, used during
+        /// recovery from failure.
+        /// For performance reasons this overload is not recommended if functions is value type (struct).
+        /// </summary>
+        /// <param name="functions">Callback functions</param>
+        /// <param name="sessionId">ID/name of previous session to resume</param>
+        /// <param name="commitPoint">Prior commit point of durability for session</param>
+        /// <param name="threadAffinitized">For advanced users. Specifies whether session holds the thread epoch across calls. Do not use with async code. Ensure thread calls session Refresh periodically to move the system epoch forward.</param>
+        /// <returns>Session instance</returns>
+        public ClientSession<Key, Value, Input, Output, Context, IFunctions<Key, Value, Input, Output, Context>> ResumeSession<Input, Output, Context>(IFunctions<Key, Value, Input, Output, Context> functions, string sessionId, out CommitPoint commitPoint, bool threadAffinitized = false)
+        {
+            return ResumeSession<Input, Output, Context, IFunctions<Key, Value, Input, Output, Context>>(functions, sessionId, out commitPoint, threadAffinitized);
         }
 
         /// <summary>
