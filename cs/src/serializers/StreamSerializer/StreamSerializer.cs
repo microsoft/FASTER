354c146175127eb06bdd736d84db048e4e5aa64a
Badrish Chandramouli <badrishc@microsoft.com>
2020-08-29 09:15:17 -0700

    Trill serializer

---
+++ cs/src/serializers/StreamSerializer/StreamSerializer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FASTER.core;
+using Microsoft.StreamProcessing.Serializer;
+using Microsoft.StreamProcessing.Sharding;
+
+namespace FASTER.serializers
+{
+    class StreamSerializer<T> : IObjectSerializer<T>
+    {
+        static StateSerializer<T> streamSerializer
+            = StreamableSerializer.Create<T>();
+        private Stream stream;
+
+        public void BeginDeserialize(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        public void BeginSerialize(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        public void Deserialize(ref T obj)
+        {
+            obj = streamSerializer.Deserialize(stream);
+        }
+
+        public void EndDeserialize()
+        {
+        }
+
+        public void EndSerialize()
+        {
+        }
+
+        public void Serialize(ref T obj)
+        {
+            streamSerializer.Serialize(stream, obj);
+        }
+    }
+}
