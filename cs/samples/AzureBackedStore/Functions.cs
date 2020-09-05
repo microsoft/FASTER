783882610326d70ccc77fc9265763ebb0e0d6d34
Badrish Chandramouli <badrishc@microsoft.com>
2020-09-05 14:12:35 -0700

    FasterKV sample using Azure storage device backend. (#314)

---
+++ cs/samples/AzureBackedStore/Functions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using FASTER.core;
+using System;
+
+namespace AzureBackedStore
+{
+    public class Functions : SimpleFunctions<long, string, string>
+    {
+        public override void ReadCompletionCallback(ref long key, ref string input, ref string output, string ctx, Status status)
+        {
+            if (status == Status.OK && output == ctx)
+                Console.WriteLine("Success!");
+            else
+                Console.WriteLine("Error!");
+        }
+    }
+}
---
+++ cs/samples/AzureBackedStore/Program.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using FASTER.core;
+using FASTER.devices;
+using System;
+using System.IO;
+
+namespace AzureBackedStore
+{
+    class Program
+    {
+        // We use emulated storage string in this sample, needs Azure Storage Emulator
+        // installed (see https://azure.microsoft.com/en-us/downloads/)
+        // You may also use a real Azure storage account here
+        public const string STORAGE_STRING = "UseDevelopmentStorage=true;";
+        public const string BASE_CONTAINER = "cloudbackedstore";
+
+        static void Main()
+        {
+            // Main hybrid log device                        
+            var log = new AzureStorageDevice(STORAGE_STRING, BASE_CONTAINER, "", "hlog.log");
+
+            // With non-blittable types, you need an object log device in addition to the
+            // main device. FASTER serializes the actual objects in the object log.
+            var objlog = new AzureStorageDevice(STORAGE_STRING, BASE_CONTAINER, "", "hlog.obj.log");
+
+            var checkpointManager = new DeviceLogCommitCheckpointManager(
+                new AzureStorageNamedDeviceFactory(STORAGE_STRING),
+                new DefaultCheckpointNamingScheme($"{BASE_CONTAINER}/checkpoints/"));
+
+
+            var store = new FasterKV<long, string>(
+                1L << 17,
+                new LogSettings {  LogDevice = log, ObjectLogDevice = objlog },
+                new CheckpointSettings { CheckpointManager = checkpointManager }
+                );
+
+            using (var s = store.NewSession(new Functions()))
+            {
+                for (long i = 0; i < 20000; i++)
+                {
+                    var _key = i;
+                    var value = $"value-{i}";
+                    s.Upsert(ref _key, ref value);
+                }
+                s.CompletePending(true);
+
+                long key = 23;
+                string output = default;
+                string context = "value-23";
+
+                var status = s.Read(ref key, ref output, context);
+
+                if (status != Status.PENDING)
+                {
+                    if (status == Status.OK && output == context)
+                        Console.WriteLine("Success!");
+                    else
+                        Console.WriteLine("Error!");
+                }
+            }
+
+            // Take fold-over checkpoint of FASTER, wait to complete
+            store.TakeFullCheckpointAsync(CheckpointType.FoldOver)
+                .GetAwaiter().GetResult();
+
+            // Dispose store instance
+            store.Dispose();
+
+            // Close logs
+            log.Close();
+            objlog.Close();
+
+            // Create new store
+            log = new AzureStorageDevice(STORAGE_STRING, BASE_CONTAINER, "", "hlog.log");
+            objlog = new AzureStorageDevice(STORAGE_STRING, BASE_CONTAINER, "", "hlog.obj.log");
+
+            var store2 = new FasterKV<long, string>(
+                1L << 17,
+                new LogSettings { LogDevice = log, ObjectLogDevice = objlog },
+                new CheckpointSettings { CheckpointManager = checkpointManager }
+                );
+
+            // Recover store from latest checkpoint
+            store2.Recover();
+            
+            using (var s = store2.NewSession(new Functions()))
+            {
+                // Test Read
+                long key = 23;
+                string output = default;
+                string context = "value-23";
+
+                var status = s.Read(ref key, ref output, context);
+
+                if (status != Status.PENDING)
+                {
+                    if (status == Status.OK && output == context)
+                        Console.WriteLine("Success!");
+                    else
+                        Console.WriteLine("Error!");
+                }
+            }
+
+            store2.Dispose();
+
+            // Purge cloud log files
+            log.PurgeAll();
+            objlog.PurgeAll();
+
+            // Purge cloud checkpoints - warning all data under specified base path are removed
+            checkpointManager.PurgeAll();
+
+            // Dispose devices
+            log.Close();
+            objlog.Close();
+
+            // Dispose checkpoint manager
+            checkpointManager.Dispose();
+
+            Console.WriteLine("Press <ENTER> to end");
+            Console.ReadLine();
+        }
+    }
+}
