bf0d3005e48e59870d05674b98d681010c48a362
Badrish Chandramouli <badrishc@microsoft.com>
2020-10-14 20:56:04 -0700

    Merge IAdvancedFunctions (changes to ClientSession.cs and FASTERThread.cs were already there)

---
+++ cs/src/core/Index/Interfaces/IAdvancedFunctions.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+
+namespace FASTER.core
+{
+    /// <summary>
+    /// Callback functions to FASTER
+    /// </summary>
+    /// <typeparam name="Key"></typeparam>
+    /// <typeparam name="Value"></typeparam>
+    /// <typeparam name="Input"></typeparam>
+    /// <typeparam name="Output"></typeparam>
+    /// <typeparam name="Context"></typeparam>
+    public interface IAdvancedFunctions<Key, Value, Input, Output, Context>
+    {
+        /// <summary>
+        /// Read completion
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        /// <param name="ctx"></param>
+        /// <param name="status"></param>
+        /// <param name="recordInfo"></param>
+        void ReadCompletionCallback(ref Key key, ref Input input, ref Output output, Context ctx, Status status, RecordInfo recordInfo);
+
+        /// <summary>
+        /// Upsert completion
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="ctx"></param>
+        void UpsertCompletionCallback(ref Key key, ref Value value, Context ctx);
+
+        /// <summary>
+        /// RMW completion
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="input"></param>
+        /// <param name="ctx"></param>
+        /// <param name="status"></param>
+        void RMWCompletionCallback(ref Key key, ref Input input, Context ctx, Status status);
+
+        /// <summary>
+        /// Delete completion
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="ctx"></param>
+        void DeleteCompletionCallback(ref Key key, Context ctx);
+
+        /// <summary>
+        /// Checkpoint completion callback (called per client session)
+        /// </summary>
+        /// <param name="sessionId">Session ID reporting persistence</param>
+        /// <param name="commitPoint">Commit point descriptor</param>
+        void CheckpointCompletionCallback(string sessionId, CommitPoint commitPoint);
+
+        /// <summary>
+        /// Initial update for RMW
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="input"></param>
+        /// <param name="value"></param>
+        void InitialUpdater(ref Key key, ref Input input, ref Value value);
+
+        /// <summary>
+        /// Whether we need to invoke copy-update for RMW
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="input"></param>
+        /// <param name="oldValue"></param>
+        bool NeedCopyUpdate(ref Key key, ref Input input, ref Value oldValue)
+#if NETSTANDARD21            
+            => true
+#endif
+            ;
+
+        /// <summary>
+        /// Copy-update for RMW
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="input"></param>
+        /// <param name="oldValue"></param>
+        /// <param name="newValue"></param>
+        void CopyUpdater(ref Key key, ref Input input, ref Value oldValue, ref Value newValue);
+
+        /// <summary>
+        /// In-place update for RMW
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="input"></param>
+        /// <param name="value"></param>
+        bool InPlaceUpdater(ref Key key, ref Input input, ref Value value);
+
+        /// <summary>
+        /// Single reader
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="input"></param>
+        /// <param name="value"></param>
+        /// <param name="dst"></param>
+        void SingleReader(ref Key key, ref Input input, ref Value value, ref Output dst);
+
+        /// <summary>
+        /// Conncurrent reader
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="input"></param>
+        /// <param name="value"></param>
+        /// <param name="dst"></param>
+        void ConcurrentReader(ref Key key, ref Input input, ref Value value, ref Output dst);
+
+        /// <summary>
+        /// Single writer
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="src"></param>
+        /// <param name="dst"></param>
+        void SingleWriter(ref Key key, ref Value src, ref Value dst);
+
+        /// <summary>
+        /// Concurrent writer
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="src"></param>
+        /// <param name="dst"></param>
+        bool ConcurrentWriter(ref Key key, ref Value src, ref Value dst);
+    }
+}
