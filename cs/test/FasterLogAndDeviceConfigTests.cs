b9a90edaf78950b0b8e363a2a82e751cb409936c
Darren Gehring <darrenge@microsoft.com>
2020-12-18 16:39:04 -0800

    Had issue with DeviceAndLogConfig that failed on Linux C# so fixed that.

--- cs/test/FasterLogAndDeviceConfigTests.cs
+++ cs/test/FasterLogAndDeviceConfigTests.cs
@@ -71,8 +71,8 @@ namespace FASTER.test
             log.Commit(true);
 
             // Verify  
-            Assert.IsTrue(File.Exists(path+"\\log-commits\\commit.0.0"));
-            Assert.IsTrue(File.Exists(path + "\\DeviceConfig.0"));
+            Assert.IsTrue(File.Exists(path+"/log-commits/commit.0.0"));
+            Assert.IsTrue(File.Exists(path + "/DeviceConfig.0"));
 
             // Read the log just to verify can actually read it
             int currentEntry = 0;
