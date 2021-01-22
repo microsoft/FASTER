665d524673395ba78df448b48ebd3ba31962c03f
Tianyu Li <litianyu@mit.edu>
2021-01-21 21:06:56 -0500

    modifications

--- cs/libdpr/samples/dpredis/dpredis/ycsb/setup_sever.ps1
+++ cs/libdpr/samples/dpredis/dpredis/ycsb/setup_sever.ps1
@@ -1,5 +1,7 @@
-Copy-Item Z:\\memurai\\dump.rdb D:\\
-Copy-Item Z:\\load_uniform_250M_raw.dat D:\\
-Copy-Item Z:\\load_zipf_250M_raw.dat D:\\
-Copy-Item Z:\\run_uniform_250M_1000M_raw.dat D:\\
-Copy-Item Z:\\run_zipf_250M_1000M_raw.dat D:\\
+Copy-Item Z:\memurai\dump.rdb -Destination D:\
+Copy-Item Z:\load_uniform_250M_raw.dat -Destination D:\
+Copy-Item Z:\load_zipf_250M_raw.dat -Destination D:\
+Copy-Item Z:\run_uniform_250M_1000M_raw.dat -Destination D:\
+Copy-Item Z:\run_zipf_250M_1000M_raw.dat -Destination D:\
+& 'C:\Program Files\Memurai\memurai-cli.exe' shutdown nosave
+& 'C:\Program Files\Memurai\memurai.exe' C:\memurai.conf
