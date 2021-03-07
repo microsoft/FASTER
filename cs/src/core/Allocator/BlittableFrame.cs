3a35319371b4abdb5c1dacb2f64be1d901085535
Badrish Chandramouli <badrishc@microsoft.com>
2021-03-06 21:00:50 -0800

    [C#] Fix recovery of hash tables, speed up snapshot recovery.
    Use snapshot as default checkpoint for benchmarks.

--- cs/src/core/Allocator/BlittableFrame.cs
+++ cs/src/core/Allocator/BlittableFrame.cs
@@ -35,7 +35,7 @@ namespace FASTER.core
 
             handles[index] = GCHandle.Alloc(tmp, GCHandleType.Pinned);
             long p = (long)handles[index].AddrOfPinnedObject();
-            pointers[index] = (p + (sectorSize - 1)) & ~(sectorSize - 1);
+            pointers[index] = (p + (sectorSize - 1)) & ~((long)sectorSize - 1);
             frame[index] = tmp;
         }
 
