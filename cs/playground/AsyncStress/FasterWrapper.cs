d381ef7d5430e15c000a6fb62bdff55ee1d6aaae
Badrish Chandramouli <badrishc@microsoft.com>
2021-04-04 12:41:19 -0700

    Added stress test based on Parallel.For

---
+++ cs/playground/AsyncStress/FasterWrapper.cs
@@ -0,0 +1,67 @@
+﻿using FASTER.core;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncStress
+{
+    public class FasterWrapper
+    {
+        private readonly FasterKV<int, int> store;
+        private readonly SimpleFunctions<int, int, Empty> _simpleFunctions;
+        private readonly ConcurrentQueue<ClientSession<int, int, int, int, Empty, SimpleFunctions<int, int, Empty>>> _sessionPool;
+        private readonly FasterKV<int, int>.ClientSessionBuilder<int, int, Empty> _clientSessionBuilder;
+
+        public int NumSessions => _sessionPool.Count;
+
+        public FasterWrapper()
+        {
+            _simpleFunctions = new SimpleFunctions<int, int, Empty>();
+            _sessionPool = new ConcurrentQueue<ClientSession<int, int, int, int, Empty, SimpleFunctions<int, int, Empty>>>();
+
+            string logDirectory ="d:/FasterLogs";
+            string logFileName = Guid.NewGuid().ToString();
+            var logSettings = new LogSettings
+            {
+                LogDevice = new ManagedLocalStorageDevice(Path.Combine(logDirectory, $"{logFileName}.log"), deleteOnClose: true),
+                PageSizeBits = 12,
+                MemorySizeBits = 13
+            };
+
+            store = new FasterKV<int, int>(1L << 20, logSettings);
+            _clientSessionBuilder = store.For(_simpleFunctions);
+        }
+
+        public async Task UpsertAsync(int key, int value)
+        {
+            var session = GetPooledSession();
+            var r = await session.UpsertAsync(key, value);
+            while (r.Status == Status.PENDING)
+                r = await r.CompleteAsync();
+            _sessionPool.Enqueue(session);
+        }
+
+        public async Task<(Status, int?)> ReadAsync(int key)
+        {
+            var session = GetPooledSession();
+            (Status, int) result = (await session.ReadAsync(key).ConfigureAwait(false)).Complete();
+            _sessionPool.Enqueue(session);
+            return result;
+        }
+
+        private ClientSession<int, int, int, int, Empty, SimpleFunctions<int, int, Empty>> GetPooledSession()
+        {
+            if (_sessionPool.TryDequeue(out var result))
+                return result;
+            return _clientSessionBuilder.NewSession<SimpleFunctions<int, int, Empty>>();
+        }
+
+        public void Dispose()
+        {
+            foreach (var session in _sessionPool)
+                session.Dispose();
+            store.Dispose();
+        }
+    }
+}
---
+++ cs/playground/AsyncStress/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+using FASTER.core;
+
+#pragma warning disable CS0162 // Unreachable code detected
+
+namespace AsyncStress
+{
+    public class Program
+    {
+        public static async Task Main() => await ProfileStore(new FasterWrapper());
+
+        private static async Task ProfileStore(FasterWrapper store)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            const int numOperations = 1_000_000;
+            const bool singleThreadUpsert = true;
+            const bool singleThreadRead = false;
+            var writeTasks = new ConcurrentDictionary<long, Task>();
+            var readTasks = new ConcurrentDictionary<long, Task<(Status, int?)>>();
+
+            // Insert
+            Console.WriteLine($"    Inserting {numOperations} records ...");
+            stopWatch.Start();
+            if (singleThreadUpsert)
+            {
+                for (int i = 0; i < numOperations; i++)
+                    await store.UpsertAsync(i, i);
+            }
+            else
+            {
+                Parallel.For(0, numOperations, key => writeTasks.TryAdd(key, store.UpsertAsync(key, key)));
+                await Task.WhenAll(writeTasks.Values).ConfigureAwait(false);
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"    Insertion complete in {stopWatch.ElapsedMilliseconds} ms");
+
+            // Read
+            Console.WriteLine($"    Reading {numOperations} records ...");
+            stopWatch.Restart();
+            if (singleThreadRead)
+            {
+                for (int i = 0; i < numOperations; i++)
+                {
+                    readTasks[i] = store.ReadAsync(i);
+                    await readTasks[i];
+                }
+            }
+            else
+            {
+                Parallel.For(0, numOperations, key => readTasks.TryAdd(key, store.ReadAsync(key)));
+                await Task.WhenAll(readTasks.Values).ConfigureAwait(false);
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"    Reads complete in {stopWatch.ElapsedMilliseconds} ms");
+
+            // Verify
+            Console.WriteLine("    Verifying read results ...");
+            Parallel.For(0, numOperations, key =>
+            {
+                (Status status, int? result) = readTasks[key].Result;
+                Assert.Equal(Status.OK, status);
+                Assert.Equal(key, result);
+            });
+
+            Console.WriteLine("    Results verified");
+
+            store.Dispose();
+        }
+    }
+}
