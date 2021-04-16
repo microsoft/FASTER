0ec9fc3385a8d7f40d76527ff40f0fa782acb267
Badrish Chandramouli <badrishc@microsoft.com>
2021-04-16 11:07:50 -0700

    add test runner in playground

---
+++ cs/playground/TstRunner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FASTER.core;
+using FASTER.test.recovery.objects;
+
+namespace TstRunner
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var test = new ObjectRecoveryTests2();
+            test.Setup();
+            test.ObjectRecoveryTest2(CheckpointType.Snapshot, 400, false).GetAwaiter().GetResult();
+            test.TearDown();
+        }
+    }
+}
