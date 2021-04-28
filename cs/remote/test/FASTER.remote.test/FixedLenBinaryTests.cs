54b5a4a12fcd900cb361f82c99cf851b4f7975f3
Badrish Chandramouli <badrishc@microsoft.com>
2021-04-27 18:52:49 -0700

    Initial checkin of test suite for remote

---
+++ cs/remote/test/FASTER.remote.test/FixedLenBinaryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using FASTER.client;
+
+namespace FASTER.remote.test
+{
+    [TestFixture]
+    public class FixedLenBinaryTests
+    {
+        FixedLenServer<long, long> server;
+        FixedLenClient<long, long> client;
+        ClientSession<long, long, long, long, long, FixedLenClientFunctions, FixedLenSerializer<long, long, long, long>> session;
+
+        [SetUp]
+        public void Setup()
+        {
+            server = new FixedLenServer<long, long>(TestContext.CurrentContext.TestDirectory + "/FixedLenBinaryTests", (a, b) => a + b);
+            client = new FixedLenClient<long, long>();
+            session = client.GetSession();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            session.Dispose();
+            client.Dispose();
+            server.Dispose();
+        }
+
+        [Test]
+        public void UpsertReadTest()
+        {
+            session.Upsert(10, 23);
+            session.CompletePending();
+            session.Read(10, userContext: 23);
+            session.CompletePending();
+            session.RMW(20, 23);
+            session.RMW(20, 23);
+            session.RMW(20, 23);
+            session.CompletePending();
+            session.Read(20, userContext: 23 * 3);
+            session.CompletePending();
+        }
+    }
+}
