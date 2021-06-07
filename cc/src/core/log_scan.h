5949a13c9b267f660a2136be1fcd9147c1e6d2c0
Konstantinos Kanellis <kkanellis@cs.wisc.edu>
2021-06-06 22:18:00 -0500

    [C++] Several bugfixes in log scan iterator
    * Now correctly switches to read from next page if the record didn't
      entirely fit in the previous one.
    
    * Fix bug where record address was wrong
    
    * Fix bug where in-disk page wasn't read due to >0 offset in passed
      address.

--- cc/src/core/log_scan.h
+++ cc/src/core/log_scan.h
@@ -63,6 +63,7 @@ class ScanIterator {
    , start(begin)
    , until(end)
    , current(start)
+   , record_address(Address::kInvalidAddress)
    , currentFrame(0)
    , completedIOs(0)
    , disk(disk)
@@ -106,65 +107,78 @@ class ScanIterator {
 
   /// Returns a pointer to the next record.
   record_t* GetNext() {
-    // We've exceeded the range over which we had to perform our scan.
-    // No work to do over here other than returning a nullptr.
-    if (current >= until) return nullptr;
+    for (int iters = 0; ; iters++) {
+      // should not execute more than 2 iters
+      if (iters == 2) assert(false);
+      // We've exceeded the range over which we had to perform our scan.
+      // No work to do over here other than returning a nullptr.
+      if (current >= until) return nullptr;
 
-    // If we're within the in-memory region, then just lookup the address,
-    // increment it and return a pointer to the record.
-    if (current >= hLog->head_address.load()) {
-      auto record = reinterpret_cast<record_t*>(hLog->Get(current));
-      current += record->size();
-      return record;
+      record_t * record;
+      bool addr_in_memory = current >= hLog->head_address.load();
+      if (!addr_in_memory) {
+        // in persistent storage
+        if ((currentFrame == 0 && current.offset() == 0) || (current == start)) {
+          // block until we load pages into the memory
+          // TODO: further optimize this (i.e. async request for next page when we move to next frame)
+          BlockAndLoad();
+        }
+        record = reinterpret_cast<record_t*>(frames[currentFrame] + current.offset());
+      }
+      else {
+        record = reinterpret_cast<record_t*>(hLog->Get(current));
+      }
+
+      if (!record->header.IsNull()) {
+        // valid record -- update next record pointer and return
+        record_address = current;
+        current += record->size();
+        return record;
+      }
+
+      // likely this record didn't fit in this page -- move to next page
+      current = Address(current.page() + 1, 0);
+      if (!addr_in_memory) {
+        // switch to the next prefetched frame
+        currentFrame = (currentFrame + 1) % numFrames;
+      }
     }
-
-    // If we're over here then we need to issue reads to persistent storage.
-    return blockAndLoad();
   }
 
   /// Returns a pointer to the next record, along with its (logical) address
   record_t* GetNext(Address& address) {
     record_t* record = GetNext();
-    address = current - record->size();
+    assert(record_address != Address::kInvalidAddress);
+    address = record_address;
     return record;
   }
 
  private:
-  /// Loads pages from persistent storage if needed, and returns a pointer
-  /// to the next record in the log.
-  record_t* blockAndLoad() {
-    // We are at the first address of the first frame in the buffer. Issue
-    // an IO to the persistent layer and then wait for it to complete.
-    if (currentFrame == 0 && current.offset() == 0) {
-      auto cb = [](IAsyncContext* ctxt, Status result, size_t bytes) {
-        assert(result == Status::Ok);
-        assert(bytes == hlog_t::kPageSize);
+  /// Loads pages from persistent storage
+  void BlockAndLoad() {
+    // Issue an IO to the persistent layer and then wait for it to complete.
+    auto cb = [](IAsyncContext* ctxt, Status result, size_t bytes) {
+      assert(result == Status::Ok);
+      assert(bytes == hlog_t::kPageSize);
 
-        CallbackContext<Context> context(ctxt);
-        context->counter->fetch_add(1);
-      };
+      CallbackContext<Context> context(ctxt);
+      context->counter->fetch_add(1);
+    };
 
-      // Issue reads to fill up the buffer and wait for them to complete.
-      for (auto i = 0; i < numFrames; i++) {
-        auto ctxt = Context(&completedIOs);
-        auto addr = current.control() + (i * hlog_t::kPageSize);
-        hLog->file->ReadAsync(addr,
-                              reinterpret_cast<void*>(frames[i]),
-                              hlog_t::kPageSize, cb, ctxt);
-      }
-
-      while (completedIOs.load() < numFrames) disk->TryComplete();
-      completedIOs.store(0);
+    // Issue reads to fill up the buffer and wait for them to complete.
+    Address load_addr = Address(current.page(), 0); // read page from start
+    for (auto i = 0; i < numFrames; i++) {
+      auto ctxt = Context(&completedIOs);
+      auto addr = load_addr.control() + (i * hlog_t::kPageSize);
+      hLog->file->ReadAsync(addr,
+                            reinterpret_cast<void*>(frames[i]),
+                            hlog_t::kPageSize, cb, ctxt);
     }
 
-    // We have the corresponding page in our buffer. Look it up, increment
-    // the current address and current frame (if necessary), return a
-    // pointer to the record.
-    auto record = reinterpret_cast<record_t*>(frames[currentFrame] +
-                                              current.offset());
-    current += record->size();
-    if (current.offset() == 0) currentFrame = (currentFrame + 1) % numFrames;
-    return record;
+    while (completedIOs.load() < numFrames) {
+      disk->TryComplete();
+    }
+    completedIOs.store(0); // reset
   }
 
   /// Passed in to the persistent layer when reading pages. Contains a pointer
@@ -211,6 +225,9 @@ class ScanIterator {
 
   /// Logical address within the log at which we are currently scanning.
   Address current;
+
+  /// Logical address of the last record we read
+  Address record_address;
 
   /// Current frame within the buffer we're scanning through for records
   /// on persistent storage.
