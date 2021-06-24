d52cc0737f349c72c1c17ad0bdcf42ca78f55019
Konstantinos Kanellis <kkanellis@cs.wisc.edu>
2021-06-24 18:20:10 -0500

    [C++] Fix compilation error

--- cc/src/core/guid.h
+++ cc/src/core/guid.h
@@ -78,7 +78,7 @@ class Guid {
 
   static bool IsNull(Guid guid) {
 #ifdef _WIN32
-    return Guid.guid == GUID_NULL;
+    return guid == GUID_NULL;
 #else
     return uuid_is_null(guid.uuid_);
 #endif
