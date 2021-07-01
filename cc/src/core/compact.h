811a72f7acb5efdb263177906b521d2f60150169
Konstantinos Kanellis <kkanellis@cs.wisc.edu>
2021-07-01 11:05:17 -0500

    [C++] Avoid key/value copying on compaction contexts

--- cc/src/core/compact.h
+++ cc/src/core/compact.h
@@ -28,41 +28,30 @@ struct CompactionThreadsContext {
   std::vector<std::unique_ptr<std::atomic<bool>>> done;
 };
 
+/// Holds the record info when a compaction request goes async
 template<class K, class V>
 struct CompactionPendingRecordEntry {
   typedef K key_t;
   typedef V value_t;
+  typedef Record<key_t, value_t> record_t;
 
-  CompactionPendingRecordEntry(const Record<K, V>* record_, const Address address_,
+  CompactionPendingRecordEntry(record_t* record_, const Address address_,
                               HashBucketEntry expected_entry_, Address search_min_offset_)
-    : key(record_->key())
-    , value(record_->value())
-    , address(address_)
-    , expected_entry(expected_entry_)
-    , search_min_offset(search_min_offset_)
-    {}
-
-  CompactionPendingRecordEntry(const K key_, const V value_, const Address address_,
-                              HashBucketEntry expected_entry_, Address search_min_offset_)
-    : key(key_)
-    , value(value_)
-    , address(address_)
-    , expected_entry(expected_entry_)
-    , search_min_offset(search_min_offset_)
+    : record{ record_ }
+    , address{ address_}
+    , expected_entry{ expected_entry_ }
+    , search_min_offset{ search_min_offset_ }
     {}
 
   CompactionPendingRecordEntry(const CompactionPendingRecordEntry& from)
-    : key(from.key)
-    , value(from.value)
-    , address(from.address)
-    , expected_entry(from.expected_entry)
-    , search_min_offset(from.search_min_offset)
+    : record{ from.record }
+    , address{ from.address }
+    , expected_entry{ from.expected_entry }
+    , search_min_offset{ from.search_min_offset }
     {}
 
-  // Key of the record
-  K key;
-  // Value of the record
-  V value;
+  // Pointer to the record
+  record_t* record;
   // Logical address in the hybrid log
   Address address;
   // Hash bucket entry expect to see when CAS the hash bucket address
@@ -72,88 +61,44 @@ struct CompactionPendingRecordEntry {
 };
 
 
-/// Exists context used by FASTER's compaction algorithm.
-///
-/// The following are template arguments.
-///    K: The type on the key of each record.
-///    V: The type on the value stored inside FASTER.
+/// Exists context used by compaction algorithm.
 template <class K, class V>
 class CompactionExists : public IAsyncContext {
  public:
-  // Typedefs on the key and value required internally by FASTER.
   typedef K key_t;
   typedef V value_t;
-
-  // Type signature on the record. Required by the constructor.
   typedef Record<K, V> record_t;
 
-
   /// Constructs and returns a context given a pointer to a record.
-  CompactionExists(const record_t* record, Address record_address,
+  CompactionExists(record_t* record, Address record_address,
           void* records_info, void* records_queue)
-   : key_(record->key())
-   , value_(record->value())
-   , address_(record_address)
-   , records_info_(records_info)
-   , records_queue_(records_queue)
+   : record_{ record }
+   , address_{ record_address }
+   , records_info_{ records_info }
+   , records_queue_{ records_queue }
   {}
 
-  /// Constructs and returns a context given key-value fields
-  CompactionExists(K key, V value, Address record_address,
-          void* records_info, void* records_queue)
-   : key_(key)
-   , value_(value)
-   , address_(record_address)
-   , records_info_(records_info)
-   , records_queue_(records_queue)
-  {}
-
-  /// Copy constructor. Required for when a Read operation goes async
-  /// inside FASTER.
+  /// Copy constructor -- required for when the Exists operation goes async
   CompactionExists(const CompactionExists& from)
-   : key_(from.key_)
-   , value_(from.value_)
-   , address_(from.address_)
-   , records_info_(from.records_info_)
-   , records_queue_(from.records_queue_)
+   : record_{ from.record_ }
+   , address_{ from.address_ }
+   , records_info_{ from.records_info_ }
+   , records_queue_{ from.records_queue_ }
   {}
 
   /// Accessor for the key. Invoked from within FASTER.
-  inline const K& key() const {
-    return key_;
+  inline const key_t& key() const {
+    return record_->key();
   }
-
-  /// Returns the size of the value. Invoked from within FASTER when creating
-  /// a new key-value pair (because the key did not map to a value to begin
-  /// with).
-  inline static constexpr uint32_t value_size() {
-    return V::size();
-  }
-
   inline Address record_address() const {
     return address_;
   }
-
   inline void* records_info() const {
     return records_info_;
   }
-
-  inline void * records_queue() const {
+  inline void* records_queue() const {
     return records_queue_;
   }
-
-  /// Stores this context's value into the passed in reference. This is
-  /// typically invoked from within FASTER when a new record corresponding
-  /// to the key-value pair is created at the tail of the hybrid log.
-  inline void Get(V& val) {
-    new(&val) V(value_);
-  }
-
-  inline bool GetAtomic(V& val) {
-    new(&val) V(value_);
-    return true;
-  }
-
 
  protected:
   /// Copies this context into a passed-in pointer if the operation goes
@@ -163,22 +108,55 @@ class CompactionExists : public IAsyncContext {
   }
 
  private:
-  /// The key the read must be performed against.
-  K key_;
-
-  /// The value that the key should map to after the Read operation.
-  V value_;
-
+  /// Pointer to the record
+  record_t* record_;
   /// The address of the record that was read
   Address address_;
-
   /// Pointer to the records info map (stored in Compact method)
   void* records_info_;
-
   /// Pointer to the pedning records queue (stored in Compact method)
   void* records_queue_;
 };
 
+/// Copy to tail context used by compaction algorithm.
+template <class K, class V>
+class CompactionCopyToTailContext {
+ public:
+  typedef K key_t;
+  typedef V value_t;
+  typedef Record<key_t, value_t> record_t;
+
+  /// Constructs and returns a context given a pointer to a record.
+  CompactionCopyToTailContext(record_t* record_)
+   : record{ record_ }
+  {}
+  /// Copy constructor deleted; copy to tail request doesn't go async
+  CompactionCopyToTailContext(const CompactionCopyToTailContext& from) = delete;
+
+  /// Accessor for the key
+  inline const key_t& key() const {
+    return record->key();
+  }
+  inline uint32_t key_size() const {
+    return record->key().size();
+  }
+  inline KeyHash get_key_hash() const {
+    return record->key().GetHash();
+  }
+  inline bool is_key_equal(const key_t& other) const {
+    return record->key() == other;
+  }
+  inline uint32_t value_size() const {
+    return record->value().size();
+  }
+
+  record_t* record;
+};
+
+
+///////////////////////////////////////////////////////////
+/// [OLD] Methods used by the old compaction method
+///////////////////////////////////////////////////////////
 
 /// Upsert context used by FASTER's compaction algorithm.
 ///
