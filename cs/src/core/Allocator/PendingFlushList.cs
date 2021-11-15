b7fb2f770eb451c875d7b5d3480d0db6106bfd7f
Badrish Chandramouli <badrishc@microsoft.com>
2021-11-15 11:25:43 -0800

    nit

--- cs/src/core/Allocator/PendingFlushList.cs
+++ cs/src/core/Allocator/PendingFlushList.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace FASTER.core
 {
-    class PendingFlushList
+    sealed class PendingFlushList
     {
         public readonly LinkedList<PageAsyncFlushResult<Empty>> list;
 
