82272184afca748b90db43d20fb4ac58120ee453
Badrish Chandramouli <badrishc@microsoft.com>
2021-11-30 18:09:08 -0800

    formatting

--- cs/src/core/Utilities/BufferPool.cs
+++ cs/src/core/Utilities/BufferPool.cs
@@ -129,7 +129,8 @@ namespace FASTER.core
         public static bool Disabled = false;
 
         /// <summary>
-        /// Unpin objects when they are returned to the pool, so that we do not hold pinned objects long term. If set, we will unpin when objects are returned and re-pin when objects are returned from the pool.
+        /// Unpin objects when they are returned to the pool, so that we do not hold pinned objects long term.
+        /// If set, we will unpin when objects are returned and re-pin when objects are returned from the pool.
         /// This static option should be enabled on program entry, and not modified once FASTER is instantiated.
         /// </summary>
         public static bool UnpinOnReturn = false;
