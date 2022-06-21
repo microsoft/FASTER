09a3a2135e5cd87e2de760dcbade1370557772a5
Konstantinos Kanellis <kkanellis@cs.wisc.edu>
2022-06-21 15:36:49 -0500

    Auxiliary methods in key hash for cold-index ops

--- cc/src/core/key_hash.h
+++ cc/src/core/key_hash.h
@@ -34,11 +34,18 @@ struct KeyHash {
 };
 static_assert(sizeof(KeyHash) == 8, "sizeof(KeyHash) != 8");
 
-template <class U, uint8_t kInChunkBits>
+template <class U, uint8_t kInChunkIndexBits, uint8_t kInChunkTagBits>
 struct IndexKeyHash {
-  // Bits used for in-chunk entries should be less than bits used for the entire address
-  static_assert(kInChunkBits < Address::kAddressBits);
-  static constexpr uint64_t kMaxInChunk = ((uint64_t)1 << kInChunkBits) - 1;
+  static_assert(kInChunkTagBits >= 0 &&  kInChunkTagBits <= 8,
+              "Chunk tag bits can cause overflow of uint8_t");
+  static_assert(kInChunkIndexBits >= 0 && kInChunkIndexBits <= 8,
+              "Chunk index bits can cause overflow of uint8_t");
+
+  static constexpr uint8_t kInChunkTotalBits = kInChunkIndexBits + kInChunkTagBits;
+  static_assert(kInChunkTotalBits < U::kReservedBits);
+
+  static constexpr uint64_t kInChunkIndexSize = ((uint64_t)1 << kInChunkIndexBits);
+  static constexpr uint64_t kInChunkTagSize = ((uint64_t)1 << kInChunkTagBits);
 
   IndexKeyHash()
     : entry_{ 0 } {
@@ -62,15 +69,8 @@ struct IndexKeyHash {
   /// Truncate the key hash's address to get the page_index into a hash table of specified size.
   inline size_t hash_table_index(uint64_t hash_table_size) const {
     assert(Utility::IsPowerOfTwo(hash_table_size));
-    return (entry_.address >> kInChunkBits) & (hash_table_size - 1);
+    return entry_.address & (hash_table_size - 1);
   }
-
-  /// Truncate the key hash's address to get the page_index into a hash table of specified size.
-  inline size_t in_chunk_index() const {
-    assert(kInChunkBits != 0); // should not be called if there are no in-chunks bits
-    return entry_.address & kInChunkBits;
-  }
-
   /// The tag serves as a discriminator inside a hash bucket. (Hash buckets use 2 bits
   /// for control and 48 bits for log-structured store offset; the remaining tag bits discriminate
   /// between different key hashes stored in the same bucket.)
@@ -78,15 +78,34 @@ struct IndexKeyHash {
     return static_cast<uint16_t>(entry_.tag);
   }
 
+  inline size_t chunk_id(uint64_t hash_table_size) const {
+    assert(kInChunkTotalBits != 0); // should not be called if there are no in-chunks bits
+    return (hash_table_index(hash_table_size) << U::kTagBits) + tag();
+  }
+  inline uint8_t index_in_chunk() const {
+    assert(kInChunkTotalBits != 0); // should not be called if there are no in-chunks bits
+    return (entry_.reserved >> kInChunkTagBits) & (kInChunkIndexSize - 1);
+  }
+  inline uint8_t tag_in_chunk() const {
+    assert(kInChunkTotalBits != 0); // should not be called if there are no in-chunks bits
+    return entry_.reserved & (kInChunkTagSize - 1);
+  }
+  inline uint64_t control() const {
+    return entry_.control;
+  }
+  inline KeyHash to_keyhash() const {
+    return KeyHash{ control() };
+  }
+
  private:
   U entry_;
 };
 
 // Hot index hash bucket definition -- no chunks
-typedef IndexKeyHash<index::HotLogIndexBucketEntryDef, 0> HotLogIndexKeyHash;
-// Cold index hash bucket definition -- 4kB chunks
+typedef IndexKeyHash<index::HotLogIndexBucketEntryDef, 0, 0> HotLogIndexKeyHash;
+// Cold index hash bucket definition -- 64 buckets in chunk, 8 entries per chunk bucket
 static_assert(Constants::kCacheLineBytes == 64);
-typedef IndexKeyHash<index::ColdLogIndexBucketEntryDef, 6> ColdLogIndexKeyHash;
+typedef IndexKeyHash<index::ColdLogIndexBucketEntryDef, 6, 3> ColdLogIndexKeyHash;
 
 }
 } // namespace FASTER::core
